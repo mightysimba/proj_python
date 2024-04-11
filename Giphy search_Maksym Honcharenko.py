331be964f26c79958ae6535b41581c690ad4feaf
Maksym Honcharenko <poppingsimba@gmail.com>
2024-04-11 21:24:26 +0000

    Update Giphy search_Maksym Honcharenko.py

--- Giphy search_Maksym Honcharenko.py
+++ Giphy search_Maksym Honcharenko.py
@@ -1,16 +1,13 @@
-# python
 import json
 from urllib import parse, request
-
 url = "http://api.giphy.com/v1/gifs/search"
-
 params = parse.urlencode({
-  "q": input("Enter you keyword:"),
+  "q": input("Enter your keyword:"),
   "api_key": "EtLMRzfmAfd8YrhD938TrpnjXHP3QE4B",
-  "limit": "2"
+  "limit": "1"
 })
-
-with request.urlopen("".join((url, "?", params))) as response:
+full_url = "".join((url, "?", params))
+with request.urlopen(full_url) as response:
     data = json.loads(response.read())
-
-print(json.dumps(data, sort_keys=True, indent=4))
+for gif in data['data']:
+    print(gif['url'])
